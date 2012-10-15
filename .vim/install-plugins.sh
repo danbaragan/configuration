9fec36d7c725fbdb19cf078ad36d8d1c759079ee
Daniel <daniel@durex>
2012-10-15 21:52:08 +0300

    Add pathogen to install procedure

--- .vim/install-plugins.sh
+++ .vim/install-plugins.sh
@@ -1,5 +1,7 @@
-mkdir ~/.vim/
-mkdir ~/.vim/{autoload,bundle}
+mkdir -p ~/.vim/
+mkdir -p ~/.vim/{autoload,bundle}
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -Sso ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 cd ~/.vim/
 git init
 

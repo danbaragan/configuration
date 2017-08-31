3f693ed77841b7cc3ebddb615f6a7c9cc2b777b3
danbaragan <daniel.mihai.baragan@gmail.com>
2017-08-31 16:36:48 +0300

    node, bash: add NODE_PATH to custom ~/.npm-global dir

--- .bash-common.sh
+++ .bash-common.sh
@@ -2,6 +2,7 @@
 # bash settings common between .bash_profile and .bashrc (both needed on mac)
 
 [ -f ~/.bashperlrc ] && . ~/.bashperlrc
+export NODE_PATH=${HOME}/.npm-global/lib/node_modules:/usr/local/lib/node_modules
 export GOPATH=$HOME/gobin
 COMMON_PATH=${HOME}/bin:${HOME}/.npm-global/bin:
 

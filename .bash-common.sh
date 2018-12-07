80320403f481779af2c97a01a7d11ae5a4387f83
danbaragan <daniel.mihai.baragan@gmail.com>
2018-12-07 20:34:28 +0200

    python3 is default in shell

--- .bash-common.sh
+++ .bash-common.sh
@@ -2,6 +2,7 @@
 # bash settings common between .bash_profile and .bashrc (both needed on mac)
 
 [ -f ~/.bashperlrc ] && . ~/.bashperlrc
+export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
 export NODE_PATH=${HOME}/.npm-global/lib/node_modules:/usr/local/lib/node_modules
 export GOPATH=$HOME/go
 PATH_COMMON=${HOME}/bin:${HOME}/.npm-global/bin:${GOPATH}/bin

50d33a7a82c5e66dba6078b4ee10dbfa176b3756
Daniel Mihai Bărăgan <daniel.mihai.baragan@gmail.com>
2019-01-14 21:22:54 +0200

    Add rust path

--- .bash-common.sh
+++ .bash-common.sh
@@ -5,5 +5,5 @@
 export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
 export NODE_PATH=${HOME}/.npm-global/lib/node_modules:/usr/local/lib/node_modules
 export GOPATH=$HOME/go
-PATH_COMMON=${HOME}/bin:${HOME}/.npm-global/bin:${GOPATH}/bin
+PATH_COMMON=${HOME}/bin:${HOME}/.npm-global/bin:${GOPATH}/bin:$HOME/.cargo/bin
 

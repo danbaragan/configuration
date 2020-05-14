d50817119653c83be24c5691b2dbffb76c19d497
Daniel Mihai Bărăgan <daniel.mihai.baragan@gmail.com>
2020-05-14 17:23:01 +0300

    bash: update nvm setup

--- .bash-common.sh
+++ .bash-common.sh
@@ -2,12 +2,14 @@
 # bash settings common between .bash_profile and .bashrc (both needed on mac)
 
 [ -f ~/.bashperlrc ] && . ~/.bashperlrc
-export NODE_PATH=${HOME}/.npm-global/lib/node_modules:/usr/local/lib/node_modules
-export GOPATH=$HOME/gobin
-COMMON_PATH=${HOME}/bin:${HOME}/.npm-global/bin:
-
-export PATH=${COMMON_PATH}:${PATH}
-
+export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
 export EDITOR=/usr/bin/vim
 export VISUAL=/usr/bin/vim
 
+export NODE_PATH=${HOME}/.npm-global/lib/node_modules:/usr/local/lib/node_modules
+export GOPATH=$HOME/gobin
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+PATH_COMMON=${HOME}/bin:${HOME}/.npm-global/bin:${GOPATH}/bin:$HOME/.cargo/bin

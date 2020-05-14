a26ebf4c6aab922ac098fa22318ef396f9b33e22
Daniel Mihai Bărăgan <daniel.mihai.baragan@gmail.com>
2020-05-14 17:23:01 +0300

    bash: update nvm setup

--- .bash-common.sh
+++ .bash-common.sh
@@ -4,8 +4,13 @@
 [ -f ~/.git-prompt.sh ] && . ~/.git-prompt.sh
 [ -f ~/.git-completion.sh ] && . ~/.git-completion.sh
 [ -f ~/.bashperlrc ] && . ~/.bashperlrc
+
 export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
 export NODE_PATH=${HOME}/.npm-global/lib/node_modules:/usr/local/lib/node_modules
 export GOPATH=$HOME/go
-PATH_COMMON=${HOME}/bin:${HOME}/.npm-global/bin:${GOPATH}/bin:$HOME/.cargo/bin
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+PATH_COMMON=${HOME}/bin:${HOME}/.npm-global/bin:${GOPATH}/bin:$HOME/.cargo/bin
